3f029334b59f762d369363887d0767149f2c6076
Roy Berner <r.p.berner@me.com>
2018-09-01 19:57:24 +0200

    Les 111

--- Instragram/AppDelegate.swift
+++ Instragram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "20adb606cf9551158345b4b09e5c21544ff4b179"
+            $0.clientKey = "39e2ac279c4445093b155828606b4b3f7aac069f"
+            $0.server = "http://ec2-18-191-166-116.us-east-2.compute.amazonaws.com/parse"
+        }
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
